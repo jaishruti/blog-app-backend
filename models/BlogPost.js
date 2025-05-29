7b9ca7722d15cb849a12d47170b26c1d17e471ec
jaishruti <shrutijaiswal223@gmail.com>
2025-05-29 12:33:33 +0530

    blog schema

---
+++ models/BlogPost.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  //multiple users can like
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  createdAt: { type: Date, default: Date.now },
+});
