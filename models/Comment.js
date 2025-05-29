a089cfe4afc720ebf423932fd3d68eb0be83a885
jaishruti <shrutijaiswal223@gmail.com>
2025-05-29 12:40:27 +0530

    comment schema

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
